2020414ac49696d806610c15fba3d31cee1975fa
Sopraj1120 <manchukansopraj@gmail.com>
2024-10-10 12:23:57 +0530

    initial commit

---
+++ Exam practice/MovieManager.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam_practice
+{
+    public class MovieManager
+    {
+        private List<Movie> movies = new List<Movie>();
+
+        public void CreateMovie(int Id, string title, string director, decimal price)
+        {
+            try
+            {
+                var movie = new Movie(Id, title, director, price);
+                movies.Add(movie);
+                Console.WriteLine("Movie Added Successfully!.");
+            }
+            catch (Exception ex)
+            {
+               Console.WriteLine(ex.Message);
+            }
+
+           
+        }
+
+        public void UpdateMovie (int id, string title, string director, decimal price)
+        {
+            var movie = movies.Find(a=> a.MovieId == id);
+
+            movie.Title = title;
+            movie.Director = director;
+            movie.RentalPrice = price;
+
+            Console.WriteLine("Movie Update SuccessFully!");
+
+            Console.ReadLine();
+            Console.Clear();
+
+        }
+
+        public void GetAllMovies()
+        {
+            if(movies == null)
+            {
+                Console.WriteLine("Movie Not Available");
+            }
+            foreach(var movie in movies)
+            {
+                Console.WriteLine(movie.ToString());
+            }
+        }
+
+        public void DeleteMovie(int id)
+        {
+            var movie = movies.Find(a=> a.MovieId==id);
+            movies.Remove(movie);
+            Console.WriteLine("Movie Successfully Deleted!");
+        }
+
+        public decimal ValidatePrice()
+        {
+            decimal price;
+            do
+            {
+                Console.WriteLine("Input Price Must be Positive value.");
+            }
+            while(!decimal.TryParse(Console.ReadLine(), out price) || price <= 0);
+            return price;
+        }
+    }
+}
---
+++ Exam practice/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Exam_practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MovieManager movieManager = new MovieManager();
+            int option;
+
+            do
+            {
+                Console.WriteLine("Movie Management System..");
+                Console.WriteLine("1. AddMovie");
+                Console.WriteLine("2. View Movies");
+                Console.WriteLine("3. Update Movie");
+                Console.WriteLine("4. Delete Movie");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Choose an Option: ");
+
+                if (!int.TryParse(Console.ReadLine(), out option) || option < 1 || option > 5)
+                {
+                    Console.WriteLine("Invalid inpu please put Valid id!.");
+                }
+
+                switch (option)
+                {
+                    case 1:
+                        AddMovie(movieManager); break;
+                    case 2:
+                        movieManager.GetAllMovies();
+                        break;
+                    case 3:
+                        UpdateMovie(movieManager); break;
+                    case 4:
+                        DeleteMovie(movieManager); break;
+                    case 5:
+                        Console.WriteLine("Exit This System!..");
+                        break;
+                }
+            }
+            while (option != 5);
+        }
+
+        static void AddMovie(MovieManager movieManager)
+        {
+            Console.Write("Enter Movie Id: ");
+            int Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Movie Title: ");
+            string Title = Console.ReadLine();
+
+            Console.Write("Enter Director: ");
+            string Director = Console.ReadLine();
+
+            decimal rentalPrice = movieManager.ValidatePrice();
+
+            movieManager.CreateMovie(Id, Title, Director, rentalPrice);
+            Console.Clear();
+        }
+
+        static void UpdateMovie(MovieManager movieManager)
+        {
+            Console.Write("Enter Movie New Id: ");
+            int Id = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Movie New Title: ");
+            string Title = Console.ReadLine();
+
+            Console.Write("Enter New Director: ");
+            string Director = Console.ReadLine();
+
+            decimal rentalPrice = movieManager.ValidatePrice();
+
+            movieManager.UpdateMovie(Id, Title, Director, rentalPrice);
+            Console.Clear();
+        }
+
+        static void DeleteMovie(MovieManager movieManager)
+        {
+            Console.Write("Enter movie Id: ");
+            int Id = int.Parse(Console.ReadLine());
+
+            movieManager.DeleteMovie(Id);
+            Console.Clear();
+        }
+    }
+}
